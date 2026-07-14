dc5abeb938dc6e9c248f37a758c9c27939150cb8
tcmmc <tcmmc.ir@gmail.com>
2026-07-14 15:01:57 +0330

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBxhXIVDhXNSisJ0v81ZtdjAZZt9vorNig",
+  authDomain: "tcm-mc-173a6.firebaseapp.com",
+  projectId: "tcm-mc-173a6",
+  storageBucket: "tcm-mc-173a6.firebasestorage.app",
+  messagingSenderId: "490709082172",
+  appId: "1:490709082172:web:6ec840d278caef9112a339"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
